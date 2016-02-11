2f56c464ba6ac5b3ab4cdd63badad8935e414b51
Andrew J DeGroot <astrophys.degroot@gmail.com>
2016-02-10 22:38:52 -0500

    changed app to application in ncaa_basketball_webapp

--- ncaa_basketball_webapp/application.py
+++ ncaa_basketball_webapp/application.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-from flask_ncaa_mbb import app
+from flask_ncaa_mbb import application
 
-app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+application.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
--- ncaa_basketball_webapp/flask_ncaa_mbb/__init__.py
+++ ncaa_basketball_webapp/flask_ncaa_mbb/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 from flask_ncaa_mbb import views
