bda02fefa70d11828fb1201943bd00b81aec6d9f
Andrew <andrew_degroot@goodyear.com>
2020-01-13 21:54:06 -0500

    added a status check function

---
+++ src/friday/main.py
@@ -0,0 +1,17 @@
+
+import argparse
+
+
+def do_status_check():
+    """
+    function to perform status checks of something
+    :return:
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print("hey")
+
+    do_status_check()
+
