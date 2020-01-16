036ea406efa89b6960432023fcd8322232db33f5
Andrew <andrew_degroot@goodyear.com>
2020-01-15 21:13:12 -0500

    new file,class for database interactions

---
+++ src/friday/data/friday_database_interactions.py
@@ -0,0 +1,12 @@
+
+import ncaa_basketball_db
+
+
+class FridayDatabaseInteractions:
+
+    def __init__(self):
+        self.a = 1
+
+
+if __name__ == "__main__":
+    instance = FridayDatabaseInteractions()
